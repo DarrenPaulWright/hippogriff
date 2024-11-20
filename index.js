9d96d225aacf97ad6cc1218dee5b54458c734b05
DarrenPaulWright <darren@darrenpaulwright.com>
2024-11-19 19:45:25 -0600

    More cleanup

--- index.js
+++ index.js
@@ -1,9 +1,5 @@
-/**
- * @name Installation
- * @private
- * @summary
- *
- * _Requires Babel 7.2+_
- */
 export { describe, bench, it, before, beforeEach, afterEach, after, when } from './src/hippogriff.js';
+/**
+ * Re-exported from type-enforcer.
+ */
 export { assert } from 'type-enforcer';
